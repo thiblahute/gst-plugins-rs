56120d23713cda6eda0c25a7c3437bafe3f0f88b
Thibault Saunier <tsaunier@igalia.com>
2023-08-31 10:44:46 -0400

    fixup! Convert nnes to flushin when src pad is flushing

--- generic/playbinpool/src/playbinpool/imp.rs
+++ generic/playbinpool/src/playbinpool/imp.rs
@@ -341,10 +341,10 @@ impl PlaybinPoolSrc {
                     } else {
                         gst::debug!(CAT, imp: self, "Pushing new caps downstream");
                         self.obj().set_caps(&c.caps().to_owned()).map_err(|e| {
-                            gst::error!(CAT, "Could not set caps: {e:?}");
                             if self.obj().src_pad().pad_flags().contains(gst::PadFlags::FLUSHING) {
                                 gst::FlowError::Flushing
                             } else {
+                                gst::error!(CAT, "Could not set caps: {e:?}");
                                 gst::FlowError::NotNegotiated
                             }
                         })?;
