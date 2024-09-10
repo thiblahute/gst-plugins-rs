0a7d3817bdff86ee6311192ac0bbb0dde4cff7e4
Thibault Saunier <tsaunier@igalia.com>
2024-09-10 18:55:31 -0300

    FIXME: uridecodepool: Disable reusing pipelines for now

--- utils/uridecodepool/src/uridecodepool/pool.rs
+++ utils/uridecodepool/src/uridecodepool/pool.rs
@@ -299,6 +299,7 @@ impl UriDecodePool {
                 && !p.seek_handler().has_eos_sample()
                 && p.requested_stream_id()
                     .map_or(false, |id| Some(id) == stream_id)
+                && false
         }) {
             gst::debug!(CAT, "Reusing the exact same pipeline for {:?}", stream_id);
             Some(state.unused_pipelines.remove(position))
